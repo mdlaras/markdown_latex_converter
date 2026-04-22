b677209ce5a9a4a8f365fb0a849a0a6db538f477
mdlaras <52792426+mdlaras@users.noreply.github.com>
2026-04-22 18:27:11 +0200

    initiate latex file and convert headers

--- markdown_latex_converter.c
+++ markdown_latex_converter.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+
+void initiateLatex(FILE * fileOut, FILE * fileIn, char * buffer){
+    fprintf(fileOut, "\\documentclass{article}\n");
+    
+    fgets(buffer, 1024, fileIn);
+    char *pTitle = strstr(buffer, "#");
+    int titleEnd = strcspn(buffer, "\n");
+    buffer[titleEnd] = '\0';
+    if (pTitle != NULL){
+        fprintf(fileOut, "\\title{%s}\n", pTitle + 1);
+    } else {
+        fprintf(fileOut, "\\title{Document}\n");
+    }
+    fprintf(fileOut, "\\begin{document}\n");
+    fprintf(fileOut, "\\maketitle\n");
+}
+
+void closeLatex(FILE * File){
+    fprintf(File, "\\end{document}\n");
+}
 
 int main(){
+    FILE * pFileInput;
+    FILE * pFileOutput;
+    char * identifier[2] = {"#", "*"};
+    char buffer[1024];
 
+    pFileInput = fopen("test.md", "r");
+    pFileOutput = fopen("test.tex", "w");
+
+    initiateLatex(pFileOutput, pFileInput, buffer);
+    while(fgets(buffer, 1024, pFileInput) != NULL){
+        char *pHeaderOne = strstr(buffer, "#");
+        char *pHeaderTwo = strstr(buffer, "##");
+        char *pItalic = strstr(buffer, "*");
+        char *pBold = strstr(buffer, "**");
+        int lineEnd = strcspn(buffer, "\n");
+        if(lineEnd < 1024){
+            buffer[lineEnd] = '\0';
+        }
+        
+        if (pHeaderTwo != NULL){
+            fprintf(pFileOutput, "\\subsection{%s}\n", pHeaderTwo + 2);
+        }
+        else if (pHeaderOne != NULL){
+            fprintf(pFileOutput, "\\section{%s}\n", pHeaderOne+1);
+        } else {
+            fprintf(pFileOutput, "%s", buffer);
+        }
+        
+    }
+
+    closeLatex(pFileOutput);
+    fclose(pFileInput);
+    fclose(pFileOutput);
     return 0;
 }
---
+++ test.md
@@ -0,0 +1,8 @@
+#This is the title
+
+# This is the first header
+
+## This is the second header
+
+I like going for a walk, just like *Sherlock Holmes*. If I could be a **priest**, I would probably be like *Father Brown*.
+
